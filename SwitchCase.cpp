44a7ba30092a938b596f27e2b23dd8bb1a4ea7f2
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-31 21:35:43 +0700

    commit2

--- SwitchCase.cpp
+++ SwitchCase.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 //Deklarasi variable global
-int kodeJurussan;
+int kodeJurusan;
 string namajurusan;
 string statuskuota;
 
 //prosedur input
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" <<endl;
+    cout << "3. Sistem Informasi" <<endl;
+    cout << "----------------------------------" << endl;
+    cin >> kodeJurusan;
